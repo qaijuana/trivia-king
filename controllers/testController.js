6b1f77ea3617a54c74bd13480771bba16c0c65e6
hermanlyx <hermanlyx@gmail.com>
2021-10-30 12:50:58 +0800

    added dummy data to be sent as json in route /test

--- controllers/testController.js
+++ controllers/testController.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("successful test!");
+  const dummyData = [
+    { name: "datapoint 1", description: "first test" },
+    { name: "datapoint 2", description: "first test part 2" },
+  ];
+  res.json(dummyData);
 });
 
 module.exports = router;
