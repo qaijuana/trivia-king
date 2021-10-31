1e22200286b9f1b57c1c29be284190f754d41abb
hermanlyx <hermanlyx@gmail.com>
2021-10-31 14:30:11 +0800

    configured Routes for pages in app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
 import "./App.css";
-import Test from "./components/Test";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+//* Import Components
+// import Test from "./components/Test";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Test />
+      <Navbar />
+      {/* <Test /> */}
+      <Switch>
+        <Route exact path="/"></Route>
+        <Route path="/login"></Route>
+        <Route path="/signup"></Route>
+        <Route path="/quiz/new"></Route>
+        <Route path="/quiz/:quizId/play"></Route>
+        <Route path="/quiz/:quizId/results"></Route>
+        <Route path="/quiz/:quizId/edit"></Route>
+        {/* View Quiz Page */}
+        <Route path="/quiz/:quizId"></Route>
+        <Route path="/user/:username/myQuizzes"></Route>
+        {/* User Profile */}
+        <Route path="/user/:username"></Route>
+        <Route>
+          <Redirect to="/" />
+        </Route>
+      </Switch>
     </div>
   );
 }
