d2a6dcd306e5172a133a68bf187cf2a8cbb8be2a
Tyler Say <sayqi@live.com>
2021-10-31 15:31:55 +0800

    sessions

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 //* Dependencies
+const dotenv = require('dotenv').config()
 const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 //? Controllers
 const testController = require("./controllers/testController");
+const userController = require("./controllers/users_controller.js");
+const session = require("express-session")
+const sessionsController = require("./controllers/sessions_controller.js");
 //
 //
 
@@ -24,6 +28,15 @@ app.use(express.static(path.join(__dirname, "./client/build")));
 app.use(express.json());
 //? Middleware for controllers
 app.use("/api/test", testController);
+app.use("/users", userController);
+app.use(
+  session({
+    secret: process.env.SECRET, //a random string do not copy this value or your stuff will get hacked
+    resave: false, // default more info: https://www.npmjs.com/package/express-session#resave
+    saveUninitialized: false, // default  more info: https://www.npmjs.com/package/express-session#resave
+  })
+);
+app.use("/sessions", sessionsController);
 //
 //
 
