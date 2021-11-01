c8e28504b2e24aca6a220a8510b14a9010a2d16c
hurrmun <61788143+hurrmun@users.noreply.github.com>
2021-11-01 18:39:46 +0800

    updated max-h to extend instead of replace

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,19 +2,18 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
     fontFamily: {
       sans: ["Poppins", "Helvetica", "sans-serif"],
     },
-    maxHeight: {
-      0: "0",
-      "1/4": "25%",
-      "1/2": "50%",
-      "3/5": "60%",
-      "3/4": "75%",
-      "4/5": "80%",
-      full: "100%",
-      "80vh": "80vh",
+    extend: {
+      // Adds a new breakpoint in addition to the default breakpoints
+      maxHeight: {
+        0: "0",
+        "1/4": "25%",
+        "1/2": "50%",
+        "3/4": "75%",
+        "80vh": "80vh",
+      },
     },
   },
   variants: {
