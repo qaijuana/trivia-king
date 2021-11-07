c136525b57c3a6008a15974c4836aba84e76beda
hurrmun <61788143+hurrmun@users.noreply.github.com>
2021-11-07 19:07:59 +0800

    show filtered trivia fix and test deployment

--- client/src/components/ShowFilteredTrivia.jsx
+++ client/src/components/ShowFilteredTrivia.jsx
@@ -5,23 +5,31 @@ const ShowFilteredTrivia = (props) => {
   const showSelected = () => {
     if (props.selectedCategories.length === 0) {
       return categoryList.map((item) => {
-        let categoryFilter = []
+        let categoryFilter = [];
         props.trivia.map((i) => {
           if (i.category === item) {
-            return categoryFilter.push(i)
+            return categoryFilter.push(i);
+          } else {
+            return null;
           }
-        })
-        return <TriviaCategorySection trivia={categoryFilter} category={item} />;
+        });
+        return (
+          <TriviaCategorySection trivia={categoryFilter} category={item} />
+        );
       });
     } else {
       return props.selectedCategories.map((item) => {
-        let categoryFilter = []
+        let categoryFilter = [];
         props.trivia.map((i) => {
           if (i.category === item) {
-            return categoryFilter.push(i)
+            return categoryFilter.push(i);
+          } else {
+            return null;
           }
-        })
-        return <TriviaCategorySection trivia={categoryFilter} category={item} />;
+        });
+        return (
+          <TriviaCategorySection trivia={categoryFilter} category={item} />
+        );
       });
     }
   };
